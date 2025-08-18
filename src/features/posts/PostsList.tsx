a4ccdef5d48493b7eba9a7d9ab9e811d6408db51
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-08-19 00:02:31 +0900

    add loading state

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,12 +2,13 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import { fetchPosts, selectPosts, selectPostsStatus } from "./postsSlice"
+import { fetchPosts, selectPosts, selectPostsStatus, selectPostsError } from "./postsSlice"
 
 export const PostsList = () => {
   const dispatch = useAppDispatch()
   const posts = useAppSelector(selectPosts)
   const postStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -15,17 +16,25 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const renderedPosts = posts.map(post => (
-    <article className="post-excerpt" key={post.id}>
-      <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-    </article>
-  ))
+  let content: React.ReactNode
+
+  if (postStatus === 'pending') {
+    content = <p>Loading...</p>
+  } else if (postStatus === 'succeeded') {
+    content = posts.map(post => (
+      <article className="post-excerpt" key={post.id}>
+        <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+      </article>
+    ))
+  } else if (postStatus === 'failed') {
+    content = <p>{postsError}</p>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
